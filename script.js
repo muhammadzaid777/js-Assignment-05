dceb64c5fc46d1a01dde0dd472c755de8ac74ffd
muhammadzaid777 <muhammadzaidabbasi123@gmail.com>
2026-08-25 03:02:39 +0500

    JS Assignment 05 ( 17-20 )

---
+++ script.js
@@ -0,0 +1,115 @@
+// QUESTION NUMBER 1
+
+//var matrix = [
+   // [],
+  //  [],
+   // []
+//];
+
+
+// QUESTION NUMBER 2
+
+//var matrix = [
+  //  [0, 1, 2, 3],
+   // [2, 0, 1, 2],
+ //   [2, 1, 0, 3]
+//];
+
+//document.write(matrix[0] + "<br>");
+//document.write(matrix[1] + "<br>");
+//document.write(matrix[2] + "<br>");
+
+
+// QUESTION NUMBER 3
+
+//for (var i = 1; i <= 10; i++) {
+//    document.write(i + "<br>");
+//}
+
+
+// QUESTION NUMBER 4
+
+//var num = prompt("Enter a number:");
+//var length = prompt("Enter table length:");
+
+//for (var i = 1; i <= length; i++) {
+    //document.write(num + " × " + i + " = " + (num * i) + "<br>");
+//}
+
+
+// QUESTION NUMBER 5
+
+///var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+//for (var i = 0; i < fruits.length; i++) {
+   // document.write(fruits[0] + "at index 0" + "<br>");
+    //  document.write(fruits[1] + "at index 1" + "<br>");
+      //  document.write(fruits[2] + "at index 2" + "<br>"); 
+      //  document.write(fruits[3] + "at index 3" + "<br>");
+          //document.write(fruits[4] + "at index 4" + "<br>");
+         // break
+//}
+
+
+// QUESTION NUMBER 6 — A
+// Counting
+
+//for (var i = 1; i <= 15; i++) {
+  //  document.write(i + "<br>");
+//}
+
+
+// QUESTION NUMBER 6 — B
+// Reverse Counting
+
+//for (var i = 10; i >= 1; i--) {
+  //  document.write(i + "<br>");
+//}
+
+
+// QUESTION NUMBER 6 — C
+// Even Numbers
+
+///for (var i = 0; i <= 20; i += 2) {
+   // document.write(i + "<br>");
+//}
+
+
+// QUESTION NUMBER 6 — D
+// Odd Numbers
+
+//for (var i = 1; i <= 19; i += 2) {
+ //   document.write(i + "<br>");
+//}
+
+
+// QUESTION NUMBER 6 — E
+// Series
+
+//for (var i = 2; i <= 20; i += 2) {
+   // document.write(i + "k" + "<br>");
+//}
+
+// QUESTION NUMBER 7
+
+
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// var userinfo = prompt("Welcome to ABC Bakery. What do you want?");
+
+// var found = false;
+
+// for (var i = 0; i < A.length; i++) {
+
+//     if (A[i] == userinfo) {
+//         found = true;
+//     }
+
+// }
+
+// if (found) {
+//     document.write(userinfo + " is available");
+// }
+// else {
+//     document.write(userinfo + " is not available");
+// }
